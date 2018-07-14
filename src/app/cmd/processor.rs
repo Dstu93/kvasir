c640fe400fa4e9642c7b849c32e0b83449a194d4
doenis <Dstu93@users.noreply.github.com>
2018-07-15 00:24:14 +0200

    tmp

--- src/app/cmd/processor.rs
+++ src/app/cmd/processor.rs
@@ -12,8 +12,10 @@ impl Processor{
     /// executes the given Command, if something bad is happening then it returns an ApplicationError
     /// (in case of on IO Error or invalid arguments etc)
     pub fn execute<'a,'b>(cmd: Command<'a,'b>) -> Result<(),ApplicationError>{
-        
-        Ok(())
+        match cmd {
+          Command::Add(args) => {Err(ApplicationError::UnknownSubCommand)},
+            _ => {Err(ApplicationError::UnknownSubCommand)}
+        }
     }
 
 }
